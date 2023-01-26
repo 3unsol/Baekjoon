62f78ee810843e537b3e9736a1f86fa8fdc75197
3unsol <122416885+3unsol@users.noreply.github.com>
2023-01-26 22:27:01 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 108 ms, Memory: 12832 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String word = sc.next();
+		System.out.print(word.indexOf("a")+ " ");
+		System.out.print(word.indexOf("b")+ " ");
+		System.out.print(word.indexOf("c")+ " ");
+		System.out.print(word.indexOf("d")+ " ");
+		System.out.print(word.indexOf("e")+ " ");
+		System.out.print(word.indexOf("f")+ " ");
+		System.out.print(word.indexOf("g")+ " ");
+		System.out.print(word.indexOf("h")+ " ");
+		System.out.print(word.indexOf("i")+ " ");
+		System.out.print(word.indexOf("j")+ " ");
+		System.out.print(word.indexOf("k")+ " ");
+		System.out.print(word.indexOf("l")+ " ");
+		System.out.print(word.indexOf("m")+ " ");
+		System.out.print(word.indexOf("n")+ " ");
+		System.out.print(word.indexOf("o")+ " ");
+		System.out.print(word.indexOf("p")+ " ");
+		System.out.print(word.indexOf("q")+ " ");
+		System.out.print(word.indexOf("r")+ " ");
+		System.out.print(word.indexOf("s")+ " ");
+		System.out.print(word.indexOf("t")+ " ");
+		System.out.print(word.indexOf("u")+ " ");
+		System.out.print(word.indexOf("v")+ " ");
+		System.out.print(word.indexOf("w")+ " ");
+		System.out.print(word.indexOf("x")+ " ");
+		System.out.print(word.indexOf("y")+ " ");
+		System.out.print(word.indexOf("z")+ " ");
+	
+		sc.close();
+	}
+}
