1ed12f48165621ac07260ae598403b997ef1b0d5
3unsol <122416885+3unsol@users.noreply.github.com>
2023-01-28 09:12:14 +0900

    [Bronze III] Title: 최댓값, Time: 112 ms, Memory: 12824 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[9];
+		for (int i = 0; i < 9; i++) {
+			int num = sc.nextInt();
+			arr[i] = num;
+		}
+		int max = 0;
+		int idx = 0;
+		for (int i = 0; i < 9; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+				idx = i;
+			}
+		}
+		System.out.println(max);
+		System.out.println(idx + 1);
+	}
+}
