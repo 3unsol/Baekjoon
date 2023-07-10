0a974df6fbb6971675542a5cc16768998662f934
3unsol <122416885+3unsol@users.noreply.github.com>
2023-07-10 23:51:39 +0900

    [Silver I] Title: 경로 찾기, Time: 324 ms, Memory: 20956 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int[][] map = new int[n][n];
+        for (int r = 0; r < n; r++) {
+            st = new StringTokenizer(br.readLine());
+            for (int c = 0; c < n; c++) {
+                map[r][c] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for(int k = 0; k < n; k++){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    if(map[i][k] == 1 && map[k][j] == 1){
+                        map[i][j] = 1;
+                    }
+                }
+            }
+        }
+        for(int r = 0; r < n; r++){
+            for(int c = 0; c < n; c++){
+                System.out.print(map[r][c] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
