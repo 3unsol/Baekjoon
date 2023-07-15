853a555af0188e779bce0dfa70d6b48dffb64e02
3unsol <122416885+3unsol@users.noreply.github.com>
2023-07-15 21:34:25 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 2288 ms, Memory: 91688 KB -BaekjoonHub

---
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int n, m;
+	static int[][] map;
+	static boolean[][] visited;
+	static int[] dr = { 0, 0, -1, 1 };
+	static int[] dc = { -1, 1, 0, 0 };
+	static int startR, startC;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken()); // 행
+		m = Integer.parseInt(st.nextToken()); // 열
+		map = new int[n][m]; // 맵
+		visited = new boolean[n][m]; // 방문처리
+		for (int r = 0; r < n; r++) {
+			st = new StringTokenizer(br.readLine());
+			for (int c = 0; c < m; c++) {
+				map[r][c] = Integer.parseInt(st.nextToken());
+				if (map[r][c] == 2) {
+					startR = r;
+					startC = c;
+					map[r][c] = 0;
+				}
+			}
+		}
+		visited[startR][startC] = true;
+		bfs(startR, startC);
+		for (int r = 0; r < n; r++) {
+			for (int c = 0; c < m; c++) {
+				if (!visited[r][c] && map[r][c] == 1) {
+					System.out.print(-1 + " ");
+				} else {
+					System.out.print(map[r][c] + " ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+	static void bfs(int r, int c) { // 너비우선탐색, 좌표 입력
+		Queue<int[]> q = new LinkedList<>(); // 큐 활용
+		q.add(new int[] { r, c }); // 큐에 좌표 입력 (class를 만들거나 배열로 만들어서 넣거나)
+		while (!q.isEmpty()) { // 큐가 빌 때까지
+			int[] tmp = q.poll(); // 맨 앞 꺼냄
+			for (int dir = 0; dir < 4; dir++) { // 델타 사방검색
+				int nr = tmp[0] + dr[dir]; // 다음 행 좌표
+				int nc = tmp[1] + dc[dir]; // 다음 열 좌표
+				if (nr < 0 || nc < 0 || nr >= n || nc >= m) { // 범위 벗어나면 넘기기
+					continue;
+				}
+				if (map[nr][nc] == 0 || visited[nr][nc]) { // 벽이거나 방문한 곳은 넘기기
+					continue;
+				}
+				q.add(new int[] { nr, nc }); // 갈 수 있는 곳이면 큐에 넣기
+				map[nr][nc] = map[tmp[0]][tmp[1]] + 1; // 이렇게 하나씩 갈 때마다 더해주면 마지막엔 몇 개 지났는지 저장됨.
+				visited[nr][nc] = true; // 방문처리
+			}
+		}
+	}
+}
