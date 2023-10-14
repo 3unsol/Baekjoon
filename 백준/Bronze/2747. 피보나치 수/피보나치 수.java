d8413404309673b8b67b9a7fa4c9b89dba3f8d3a
3unsol <122416885+3unsol@users.noreply.github.com>
2023-10-14 23:49:16 +0900

    [Bronze II] Title: 피보나치 수, Time: 80 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int n;
+    public static void main(String[] args) throws IOException {
+        input();
+    }
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        if(n >= 2){
+            dp[2] = 1;
+            for(int i = 3; i <= n; i++){
+                dp[i] = dp[i - 2] + dp[i - 1];
+            }
+        }
+        System.out.println(dp[n]);
+    }
+
+}
