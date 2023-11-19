63a0145f9ccc083d0c6deaa9b87c0e02a7ae02c6
3unsol <122416885+3unsol@users.noreply.github.com>
2023-11-19 17:11:20 +0900

    [Silver V] Title: 거스름돈, Time: 88 ms, Memory: 11988 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] dp = new int[100001];
+
+		dp[1] = -1;
+		dp[2] = 1;
+		dp[3] = -1;
+		dp[4] = 2;
+		dp[5] = 1;
+
+		for(int i = 6; i <= N; i++) {
+			dp[i] = Integer.MAX_VALUE;
+		}
+
+		for(int i = 6; i <= N; i++){
+				if(dp[i - 2] == -1 && dp[i - 5] == -1){
+					dp[i] = -1;
+				}
+				else if(dp[i - 2] == -1){
+					dp[i] = dp[i - 5] + 1;
+				}
+				else if(dp[i - 5] == -1){
+					dp[i] = dp[i - 2] + 1;
+				}
+				else {
+					dp[i] = Math.min(dp[i - 2], dp[i - 5]) + 1;
+				}
+			}
+		System.out.println(dp[N]);
+	}
+}
