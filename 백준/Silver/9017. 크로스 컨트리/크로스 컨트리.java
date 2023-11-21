a3e23be6ecbf67b8e2e2504533fcf1286daa9cb3
3unsol <122416885+3unsol@users.noreply.github.com>
2023-11-21 16:56:14 +0900

    [Silver III] Title: 크로스 컨트리, Time: 112 ms, Memory: 13204 KB -BaekjoonHub

--- 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.java
+++ 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.java
@@ -5,7 +5,7 @@ import java.util.StringTokenizer;
 public class Main {
 
 	static BufferedReader br;
-	static int N;
+	static int N, maxTeamNumber;
 	static int[] race, teamHeadCount, teamScore, fifthScore;
 	static boolean[] isOverSix;
 	public static void main(String[] args) throws Exception {
@@ -25,19 +25,24 @@ public class Main {
 		N = Integer.parseInt(br.readLine());
 		race = new int[N];
 		teamHeadCount = new int[201];
-		isOverSix = new boolean[201];
-		teamScore = new int[201];
-		fifthScore = new int[201];
+
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		maxTeamNumber = 0;
 		for(int i = 0; i < N; i++){
-			int tmp = Integer.parseInt(st.nextToken());
-			race[i] = tmp;
-			teamHeadCount[tmp]++;
+			int nowTeamNumber = Integer.parseInt(st.nextToken());
+			maxTeamNumber = Math.max(maxTeamNumber, nowTeamNumber);
+			race[i] = nowTeamNumber;
+			teamHeadCount[nowTeamNumber]++;
 		}
+
+		isOverSix = new boolean[maxTeamNumber + 1];
+		teamScore = new int[maxTeamNumber + 1];
+		fifthScore = new int[maxTeamNumber + 1];
+
 	}
 	static void isPossible(){
-		for(int i = 1; i < 201; i++){
+		for(int i = 1; i <= maxTeamNumber; i++){
 			if(teamHeadCount[i] >= 6){
 				isOverSix[i] = true;
 			}
@@ -45,16 +50,18 @@ public class Main {
 	}
 
 	static void race(){
-		int[] tmpCnt = new int[201];
+		int[] tmpCnt = new int[maxTeamNumber + 1];
 		int rank = 1;
+
 		for(int i = 0; i < N; i++){
 			if(isOverSix[race[i]]) {
 				tmpCnt[race[i]]++;
-				if (tmpCnt[race[i]] <= 4) {
+
+				if (tmpCnt[race[i]] <= 4)
 					teamScore[race[i]] += rank;
-				} else if (tmpCnt[race[i]] == 5) {
+				else if (tmpCnt[race[i]] == 5)
 					fifthScore[race[i]] = rank;
-				}
+
 				rank++;
 			}
 		}
@@ -63,7 +70,7 @@ public class Main {
 	static int check(){
 
 		int minScore = Integer.MAX_VALUE;
-		for(int i = 0; i < 201; i++){
+		for(int i = 0; i <= maxTeamNumber; i++){
 			if(teamScore[i] == 0){
 				teamScore[i] = Integer.MAX_VALUE;
 			}
@@ -72,7 +79,7 @@ public class Main {
 
 		int min = Integer.MAX_VALUE;
 		int winner = 0;
-		for(int i = 0; i < 201; i++){
+		for(int i = 0; i <= maxTeamNumber; i++){
 			if(teamScore[i] == minScore){
 				if(min > fifthScore[i]){
 					min = fifthScore[i];
