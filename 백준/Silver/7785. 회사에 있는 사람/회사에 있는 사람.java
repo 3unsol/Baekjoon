f1129825ba4e8f4018009fe7360c454d6e0f4aba
3unsol <122416885+3unsol@users.noreply.github.com>
2024-01-07 23:10:03 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 620 ms, Memory: 52892 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -7,7 +7,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        HashMap<String, String> company = new HashMap<>();
+        HashSet<String> company = new HashSet<>();
         StringTokenizer st;
 
         for(int i = 0; i < n; i++){
@@ -16,15 +16,16 @@ public class Main {
             String name = st.nextToken();
             String status = st.nextToken();
 
-            if(company.containsKey(name)){
-                company.remove(name);
+            if(status.equals("enter")) {
+                company.add(name);
             }else{
-                company.put(name, status);
+                company.remove(name);
             }
         }
 
-        // keySet() : 해시맵을 순회하면서 key값만 모아놓은 set 만들어준다.
-        ArrayList<String> names = new ArrayList<>(company.keySet());
+        // ArrayList 자리 만들어줄 때... 괄호 안에 넣을 집합 써주면 그거대로 해주는 듯
+        // 용어 공부하자ㅎ..
+        ArrayList<String> names = new ArrayList<>(company);
         Collections.sort(names, Collections.reverseOrder());
 
         StringBuilder sb = new StringBuilder();
