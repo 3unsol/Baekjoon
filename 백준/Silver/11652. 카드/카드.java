2dff45a0761141f5bc9508055ae2f5e174f43aed
3unsol <122416885+3unsol@users.noreply.github.com>
2024-01-14 10:43:32 +0900

    [Silver IV] Title: 카드, Time: 360 ms, Memory: 37392 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -1,15 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
         long max = 0;
         Map<Long, Long> map = new HashMap<>();
 
         for(int i = 0; i < n; i++){
-            long num = sc.nextLong();
+            long num = Long.parseLong(br.readLine());
             map.put(num, map.getOrDefault(num, 0l) + 1);
             max = Math.max(max, map.get(num));
         }
