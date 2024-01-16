786d5dab6ba0fd9af3a243f248d31fc0f42ef7bb
3unsol <122416885+3unsol@users.noreply.github.com>
2024-01-16 22:32:28 +0900

    [Gold V] Title: LCS, Time: 104 ms, Memory: 15852 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static String str1, str2;
+    static int[][] dp;
+
+    public static void main(String[] args) throws Exception {
+        input();
+        findLCS();
+        System.out.println(dp[str1.length()][str2.length()]);
+    }
+
+    static void input() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        str1 = br.readLine();
+        str2 = br.readLine();
+        dp = new int[str1.length() + 1][str2.length() + 1];
+    }
+
+    static void findLCS() {
+        for(int i = 1; i <= str1.length(); i++) {
+            for(int j = 1; j <= str2.length(); j++) {
+
+                if(str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+    }
+}
