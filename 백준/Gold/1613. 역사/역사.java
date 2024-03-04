adb8e0a7e3f4d40d96184e766713aed78bc393b2
3unsol <122416885+3unsol@users.noreply.github.com>
2024-03-04 14:23:46 +0900

    [Gold III] Title: 역사, Time: 1304 ms, Memory: 44436 KB -BaekjoonHub

---
+++ 백준/Gold/1613. 역사/역사.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static int[][] dist;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        dist = new int[N + 1][N + 1];
+
+        for(int r = 1; r <= N; r++) {
+            dist[r][r] = 0;
+        }
+
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int before = Integer.parseInt(st.nextToken());
+            int after = Integer.parseInt(st.nextToken());
+
+            dist[before][after] = -1;
+            dist[after][before] = 1;
+        }
+
+        for(int k = 1; k <= N; k++) {
+            for(int i = 1; i <= N; i++) {
+                for(int j = 1; j <= N; j++) {
+                    if(dist[i][k] == -1 && dist[k][j] == -1) {
+                        dist[i][j] = -1;
+                    }else if(dist[i][k] == 1 && dist[k][j] == 1) {
+                        dist[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        int s = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < s; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(dist[a][b]);
+        }
+    }
+}
