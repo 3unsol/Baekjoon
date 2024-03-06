e2b76d64344b78e2cd67f2dac31c7b13fb41eedf
3unsol <122416885+3unsol@users.noreply.github.com>
2024-03-06 17:54:07 +0900

    [Silver II] Title: 좌표 압축, Time: 1896 ms, Memory: 359380 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,38 +1,42 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.StringBufferInputStream;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
-		int[] sorted = new int[n];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < n; i++) {
-			arr[i] = sorted[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(sorted);
-		Map<Integer, Integer> map = new HashMap<>();
-		int rank = 0;
-		for(int i = 0; i < n; i++) {
-			if(!map.containsKey(sorted[i])) {				
-				map.put(sorted[i], rank);
-				rank++;
-			}
-		}
-		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < n; i++) {
-			sb.append(map.get(arr[i])).append(" ");
-		}
-		System.out.println(sb);
-	}
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] sorted = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < N; i++) {
+            sorted[i] = arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(sorted);
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int rank = 0;
+
+        for(int i = 0; i < N; i++) {
+            if(!map.containsKey(sorted[i])) {
+                map.put(sorted[i], rank);
+                rank++;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < N; i++) {
+            sb.append(map.get(arr[i])).append(" ");
+        }
+
+        System.out.println(sb);
+    }
 }
