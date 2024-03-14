1f0acfa2a2b7b91ba034a3f400bc66d10b4ef9e2
3unsol <122416885+3unsol@users.noreply.github.com>
2024-03-14 16:07:01 +0900

    [Silver I] Title: 기타리스트, Time: 100 ms, Memory: 13256 KB -BaekjoonHub

--- 백준/Silver/1495. 기타리스트/기타리스트.java
+++ 백준/Silver/1495. 기타리스트/기타리스트.java
@@ -1,60 +1,57 @@
-import java.io.*;
-import java.util.*;
+import java.awt.print.Pageable;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N, S, M;
-	static int[] arr;
+    static int N, S, M;
+    static int[] dp;
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        N = Integer.parseInt(st.nextToken()); // 음악 개수
+        S = Integer.parseInt(st.nextToken()); // 시작 볼륨
+        M = Integer.parseInt(st.nextToken()); // 최대 볼륨
 
-		String[] input = br.readLine().split(" ");
-		N = Integer.parseInt(input[0]);
-		S = Integer.parseInt(input[1]);
-		M = Integer.parseInt(input[2]);
+        dp = new int[M + 1]; // 인덱스 : 볼륨 크기, 값 : 음악 번호
+        for(int i = 0; i <= M; i++) {
+            dp[i] = -1;
+        }
+        dp[S] = 0; // 첫 번째 음악은 시작 볼륨
 
-		arr = new int[M + 1];
+        st = new StringTokenizer(br.readLine(), " ");
 
-		for (int i = 0; i <= M; i++) {
-			arr[i] = -1;
-		}
-		arr[S] = 0;
-
-		input = br.readLine().split(" ");
-
-		for (int i = 1; i <= N; i++) {
-			int V = Integer.parseInt(input[i - 1]);
-			List<Integer> list = new ArrayList<>();
-			
-			for (int j = 0; j <= M; j++) {
-				if (arr[j] == i - 1) { // i-1번째에서 가능한 볼륨들을 찾는다.
-					int res1 = j + V;
-					int res2 = j - V;
-
-					if (0 <= res1 && res1 <= M)
-						list.add(res1);
-					if (0 <= res2 && res2 <= M)
-						list.add(res2);
-				}
-			}
-			
-			for (int n : list) {
-				arr[n] = i;
-			}
-		}
-
-		int max = -1;
-		for (int i = 0; i <= M; i++) {
-			if (arr[i] == N)
-				max = Math.max(max, i);
-		}
-
-		bw.write(max + "\n");
-		bw.flush();
-
-	}
-
+        for(int i = 1; i <= N; i++) {
+            int range = Integer.parseInt(st.nextToken()); // 조절 가능 범위
+            List<Integer> list = new ArrayList<>(); // 가능한 모든 볼륨을 담을 리스트
+            for(int j = 0; j <= M; j++) {
+                if(dp[j] == i - 1) { // 이전 음악 가능한 볼륨 크기
+                    int up = j + range; // 상한선
+                    int down = j - range; // 하한선
+                    if(up <= M) {
+                        list.add(up);
+                    }
+                    if(0 <= down) {
+                        list.add(down);
+                    }
+                }
+            }
+            for(int vol : list) {
+                dp[vol] = i; // dp배열에 가능하다고 표시
+            }
+        }
+        for(int i = M; i >= 0; i--) {
+            if(dp[i] == N) {
+                System.out.println(i);
+                return;
+            }
+        }
+        System.out.println(-1);
+    }
 }
