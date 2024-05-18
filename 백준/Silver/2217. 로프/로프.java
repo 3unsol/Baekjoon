afeccad48a0c36d762aff99022f410ede5c90a09
3unsol <122416885+3unsol@users.noreply.github.com>
2024-05-18 21:28:22 +0900

    [Silver IV] Title: 로프, Time: 240 ms, Memory: 23340 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        int max = arr[N - 1];
+        int index = 1;
+
+        for(int i = N - 1; i >= 0; i--) {
+
+            int now = arr[i] * index;
+
+            max = Math.max(max, now);
+
+            index++;
+
+        }
+
+        System.out.println(max);
+    }
+}
