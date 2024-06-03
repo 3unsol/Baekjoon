0e57245447f313518c26549fc8cb6534902f1b7f
3unsol <122416885+3unsol@users.noreply.github.com>
2024-06-03 18:06:48 +0900

    [Silver II] Title: 생태학, Time: 472 ms, Memory: 89752 KB -BaekjoonHub

--- 백준/Silver/4358. 생태학/생태학.java
+++ 백준/Silver/4358. 생태학/생태학.java
@@ -6,9 +6,9 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
+        
         Map<String, Integer> map = new HashMap<>();
-
+        
         int total = 0;
         String name;
 
@@ -21,7 +21,8 @@ public class Main {
 
         Collections.sort(list);
 
-        for(String tree : list) {
+        for(int i = 0; i < list.size(); i++) {
+            String tree = list.get(i);
             int cnt = map.get(tree) * 100;
             sb.append(tree + " " + String.format("%.4f", (double)cnt / (double)total) + "\n");
         }
