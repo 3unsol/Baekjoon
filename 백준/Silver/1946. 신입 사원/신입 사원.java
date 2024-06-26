b1278b2e15aa5b59fe5950ddc864db0efa0477f5
3unsol <122416885+3unsol@users.noreply.github.com>
2024-06-26 16:33:23 +0900

    [Silver I] Title: 신입 사원, Time: 848 ms, Memory: 298520 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -6,7 +6,7 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int N, cnt;
-    static Node[] rank;
+    static int[] rank;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,38 +15,24 @@ public class Main {
         for(int tc = 1; tc <= testCase; tc++) {
 
             N = Integer.parseInt(br.readLine());
-            rank = new Node[N];
+            rank = new int[N + 1];
 
             for(int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
-                rank[i] = new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+                rank[Integer.parseInt(st.nextToken())] = Integer.parseInt(st.nextToken());
             }
 
-            Arrays.sort(rank);
             cnt = 1;
-            int minInterview = rank[0].interview;
+            int minInterview = rank[1];
 
-            for(int i = 1; i < N; i++) {
-                if(rank[i].interview < minInterview) {
+            for(int i = 2; i <= N; i++) {
+                if(rank[i] <= minInterview) {
                     cnt++;
-                    minInterview = rank[i].interview;
+                    minInterview = rank[i];
                 }
             }
 
             System.out.println(cnt);
-        }
-    }
-
-    static class Node implements Comparable<Node> {
-        int coverLetter, interview;
-        Node(int coverLetter, int interview) {
-            this.coverLetter = coverLetter;
-            this.interview = interview;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return this.coverLetter - o.coverLetter;
         }
     }
 }
