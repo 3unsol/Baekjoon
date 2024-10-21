ee05b806094ed9b18f5b2539f67837bccabf4975
3unsol <122416885+3unsol@users.noreply.github.com>
2024-10-21 18:23:53 +0900

    [Silver III] Title: N과 M (7), Time: 580 ms, Memory: 219996 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static int[] arr, result;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        result = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        perm(0);
+        System.out.print(sb);
+
+    }
+
+    static void perm(int depth) {
+
+        if(depth == M) {
+            for(int i = 0; i < M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 0; i < N; i++) {
+            result[depth] = arr[i];
+            perm(depth + 1);
+        }
+    }
+}
