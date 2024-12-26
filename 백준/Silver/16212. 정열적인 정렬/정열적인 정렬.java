f2ffc7712fcb1533980692f29e8f0550fd8325af
3unsol <122416885+3unsol@users.noreply.github.com>
2024-12-26 14:00:30 +0900

    [Silver V] Title: 정열적인 정렬, Time: 920 ms, Memory: 119852 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Silver/16212. 정열적인 정렬/정열적인 정렬.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N; i++) {
+            sb.append(arr[i]).append(" ");
+        }
+        System.out.print(sb);
+    }
+}
